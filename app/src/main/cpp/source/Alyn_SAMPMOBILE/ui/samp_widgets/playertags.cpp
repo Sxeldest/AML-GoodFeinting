6c17fe508a8115b7e176d3e4e73bcf69d37c5756
Scheldest-Audit <db-audit@github.com>
2026-08-09 19:59:03 +0000

    Update Playertags Logic

--- app/src/main/cpp/source/Alyn_SAMPMOBILE/ui/samp_widgets/playertags.cpp
+++ app/src/main/cpp/source/Alyn_SAMPMOBILE/ui/samp_widgets/playertags.cpp
@@ -38,19 +38,16 @@ void PlayerTags::render(ImGuiRenderer* renderer)
 
 void PlayerTags::drawTag(ImGuiRenderer* renderer, PLAYERID playerId, CRemotePlayer* pPlayer, VECTOR* pos, float fDist)
 {
-    // Line of Sight check
     VECTOR camPos;
     camPos.X = *(float*)(SA_Addr(0x9528D4));
     camPos.Y = *(float*)(SA_Addr(0x9528D8));
     camPos.Z = *(float*)(SA_Addr(0x9528DC));
 
-    // CWorld::GetIsLineOfSightClear
     bool bClear = ((bool (*)(VECTOR*, VECTOR*, bool, bool, bool, bool, bool, bool, bool)) (SA_Addr(0x423418 + 1)))(&camPos, pos, true, true, false, true, true, false, false);
 
     if (!bClear) return;
 
     VECTOR Out;
-    // CSprite::CalcScreenCoors
     bool bVisible = ((bool (*)(VECTOR*, VECTOR*, float*, float*, bool, bool)) (SA_Addr(0x5C5798 + 1)))(pos, &Out, nullptr, nullptr, false, false);
 
     if (!bVisible || Out.Z < 1.0f) return;
@@ -63,62 +60,51 @@ void PlayerTags::drawTag(ImGuiRenderer* renderer, PLAYERID playerId, CRemotePlay
     char szTag[64];
     sprintf(szTag, "%s (%d)", SAMP::netgame()->m_pools->playerPool->getPlayerName(playerId), playerId);
 
-    ImVec2 textPos = ImVec2(Out.X, Out.Y);
     ImVec2 textSize = renderer->calculateTextSize(szTag, fontSize);
-
-    textPos.x -= textSize.x * 0.5f;
-    textPos.y -= textSize.y;
+    ImVec2 textPos = ImVec2(Out.X - (textSize.x * 0.5f), Out.Y - textSize.y);
 
     renderer->drawText(textPos, UI::fixcolor(pPlayer->getPlayerColor()), szTag, true, fontSize);
 
-    uint32_t colorHealthBar    = 0xFF2822C3;
-    uint32_t colorHealthBarBG  = 0xFF181164;
-    uint32_t colorArmourBar    = 0xFFD1D1D1;
-    uint32_t colorArmourBarBG  = 0xFF282828;
+    uint32_t colorHealthBar    = 0xFF2822B9; // RGB(185, 34, 40)
+    uint32_t colorHealthBarBG  = 0xFF140B4B; // RGB(75, 11, 20)
+    uint32_t colorArmourBar    = 0xFFC8C8C8; // RGB(200, 200, 200)
+    uint32_t colorArmourBarBG  = 0xFF282828; // RGB(40, 40, 40)
     uint32_t colorBorder       = 0xFF000000;
 
-    float halfWidth    = 19.0f;
-    float barWidth     = halfWidth * 2.0f;
+    float innerWidth   = 38.0f;
     float barHeight    = 4.0f;
-    float borderOffset = 1.0f;
-    if (borderOffset < 1.0f) borderOffset = 1.0f;
 
-    ImVec2 barPos = ImVec2(Out.X - halfWidth, (float)((int)(Out.Y + 3.0f)));
+    ImVec2 barPos = ImVec2(Out.X - 19.0f, (float)((int)(Out.Y + 3.0f)));
 
     float health = pPlayer->getHealth();
     float armour = pPlayer->getArmour();
+    float healthOffset = (armour > 0.0f) ? 8.0f : 0.0f;
 
-    if (armour > 1.0f) {
+    if (armour > 0.0f) {
         if (armour > 100.0f) armour = 100.0f;
-        float aProgress = (armour / 100.0f) * barWidth;
+        float aProgress = (armour / 100.0f) * innerWidth;
 
-        // Armour Border
         renderer->drawRect(
-            ImVec2(barPos.x - borderOffset, barPos.y - borderOffset),
-            ImVec2(barPos.x + barWidth + borderOffset, barPos.y + barHeight + borderOffset),
-            colorBorder, true);
+                ImVec2(barPos.x - 1.0f, barPos.y - 1.0f),
+                ImVec2(barPos.x + innerWidth + 2.0f, barPos.y + barHeight + 1.0f),
+                colorBorder, true);
 
-        // Armour Background
-        renderer->drawRect(barPos, ImVec2(barPos.x + barWidth, barPos.y + barHeight), colorArmourBarBG, true);
-
-        // Armour Progress
+        renderer->drawRect(barPos, ImVec2(barPos.x + innerWidth, barPos.y + barHeight), colorArmourBarBG, true);
         renderer->drawRect(barPos, ImVec2(barPos.x + aProgress, barPos.y + barHeight), colorArmourBar, true);
-        barPos.y += 6.5f;
     }
 
-    // Health Border
+    ImVec2 hBarPos = ImVec2(barPos.x, barPos.y + healthOffset);
+
     renderer->drawRect(
-        ImVec2(barPos.x - borderOffset, barPos.y - borderOffset),
-        ImVec2(barPos.x + barWidth + borderOffset, barPos.y + barHeight + borderOffset),
-        colorBorder, true);
+            ImVec2(hBarPos.x - 1.0f, hBarPos.y - 1.0f),
+            ImVec2(hBarPos.x + innerWidth + 2.0f, hBarPos.y + barHeight + 1.0f),
+            colorBorder, true);
 
-    // Health Background
-    renderer->drawRect(barPos, ImVec2(barPos.x + barWidth, barPos.y + barHeight), colorHealthBarBG, true);
+    renderer->drawRect(hBarPos, ImVec2(hBarPos.x + innerWidth, hBarPos.y + barHeight), colorHealthBarBG, true);
 
-    // Health Progress
     if (health > 0.0f) {
         if (health > 100.0f) health = 100.0f;
-        float hProgress = (health / 100.0f) * barWidth;
-        renderer->drawRect(barPos, ImVec2(barPos.x + hProgress, barPos.y + barHeight), colorHealthBar, true);
+        float hProgress = (health / 100.0f) * innerWidth;
+        renderer->drawRect(hBarPos, ImVec2(hBarPos.x + hProgress, hBarPos.y + barHeight), colorHealthBar, true);
     }
 }
